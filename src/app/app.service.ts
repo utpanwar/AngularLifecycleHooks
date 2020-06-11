870d5652889b15a59810b05c4ce209ef1905e980
Utkarsh Panwar <utkarshpanwar212@gmail.com>
2020-06-12 02:11:46 +0530

    finished all task after this start with new

---
+++ src/app/app.service.ts
@@ -0,0 +1,46 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Subject, Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AppService {
+
+  items = new BehaviorSubject({
+                               id: 1, 
+                               name: 'todolist',
+                               description: 'This is todo list app'
+                              });
+
+  totalItems: any[] = [{
+                        id: 1,
+                        name: 'todolist',
+                        description: 'This is todo list app'
+                      }];
+
+  constructor() {
+    console.log(this.items); 
+    this.items.next(this.totalItems[0]);
+    console.log(this.items);
+     console.log("hii");
+     console.log(this.totalItems[0]);
+    //  console.log("hi");
+  }
+
+  setDeletedItems(deletedItem: any) {
+    console.log('deleting item::::' + deletedItem);
+    this.totalItems = this.totalItems.filter(item => item.id !== deletedItem.id);
+  }
+
+  addItems(item: any) {
+    const totalItemsLength = this.totalItems.length;
+    item.id = totalItemsLength + 1;
+    this.totalItems.push(item);
+    this.items.next(item);
+  }
+
+  get ttlItems(): any[] {
+     return this.totalItems;
+  }
+
+}
